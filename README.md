0b5021f2588356711554473e3258236e45293664
Naresh Rayapati <naresh.rayapati@yahoo.com>
2019-07-14 20:32:12 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Test Repository
+
+Test repo to checkout during the KCDC KC 2019 workshop
---
+++ jenkins/release.groovy
@@ -0,0 +1,13 @@
+def prepare() {
+    echo "Prepare: You loaded this from Git!"
+}
+
+def release() {
+    echo "Release: You loaded this from Git!"
+}
+
+def clean() {
+    echo "Clean: You loaded this from Git!"
+}
+
+return this;
